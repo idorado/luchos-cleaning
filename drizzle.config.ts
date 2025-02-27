30dcda8867778b56946c3ae4bcd76193ada3c300
Jose Sanchez <engineer@copydataai.com>
2025-02-27 11:15:46 +0000

    fix(config): update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
-export default {
-  schema: './src/db/schema/*',
-  out: './src/db/migrations',
+export default defineConfig({
+  schema: './src/server/db/schema/index',
+  out: './src/server/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL ?? "",
   },
-} satisfies Config
+})
