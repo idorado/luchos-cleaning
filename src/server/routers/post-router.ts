e347bc09c8b62b3d83eb1e923285f6d8b0605651
Jose Sanchez <engineer@copydataai.com>
2025-02-27 10:40:27 +0000

    chore(server): add post-router

---
+++ src/server/routers/post-router.ts
@@ -0,0 +1,24 @@
+import { desc } from "drizzle-orm"
+import { z } from "zod"
+import { j, publicProcedure } from "../jstack"
+
+const posts = [{name: "hello"}]
+
+export const postRouter = j.router({
+  recent: publicProcedure.query(async ({ c, ctx }) => {
+    console.log(posts);
+    return c.superjson(posts)
+  }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, c, input }) => {
+      const { name } = input
+
+      console.log("prev", posts)
+      posts.push({name})
+      console.log("later", posts)
+
+      return c.superjson(posts[posts.length]);
+    }),
+})
