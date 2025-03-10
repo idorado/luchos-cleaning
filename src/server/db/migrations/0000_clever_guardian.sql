54872ca552ec6268276ee39ce6e5503d9b9d0234
Jose Sanchez <engineer@copydataai.com>
2025-03-10 13:59:54 +0000

    feat(db): add migration services & testimonials

---
+++ src/server/db/migrations/0000_clever_guardian.sql
@@ -0,0 +1,19 @@
+CREATE TYPE "public"."service_category" AS ENUM('residential', 'commercial', 'emergency');--> statement-breakpoint
+CREATE TABLE "services" (
+	"id" text PRIMARY KEY NOT NULL,
+	"service_name" text NOT NULL,
+	"category" "service_category" NOT NULL,
+	"type" varchar(50) NOT NULL,
+	"description" text,
+	"price" numeric(10, 2) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now(),
+	"updated_at" timestamp with time zone DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "testimonials" (
+	"id" text PRIMARY KEY NOT NULL,
+	"quote" text NOT NULL,
+	"author" text NOT NULL,
+	"role" text NOT NULL,
+	"service" text NOT NULL
+);
