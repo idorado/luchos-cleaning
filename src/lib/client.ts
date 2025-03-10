e6c4183d66b01b2f07bfc14e0eb9f44d93f465f9
Jose Sanchez <engineer@copydataai.com>
2025-03-10 22:30:28 +0000

    fix(lib): update client for deploy on vercel

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -6,5 +6,18 @@ import { createClient } from "jstack"
  * @see https://jstack.app/docs/backend/api-client
  */
 export const client = createClient<AppRouter>({
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: `${getBaseURL()}/api`,
 })
+
+
+function getBaseURL() {
+  if (typeof window !== 'undefined'){
+    return window.location.origin
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  return 'http://localhost:3000';
+} 
