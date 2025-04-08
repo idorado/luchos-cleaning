1242565dc3e74523f109a3beef0a9bf73030f1d4
Jose Sanchez <engineer@copydataai.com>
2025-04-08 23:37:44 +0100

    fix(db): update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
+
+const postgresURL = process.env.DATABASE_URL || "";
+
+const nonPoolingUrl = postgresURL.replace(":6543", ":5432");
 
 export default defineConfig({
-  schema: './src/server/db/schema',
-  out: './src/server/db/migrations',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
-  },
-})
+	schema: "./src/server/db/schema",
+	out: "./src/server/db/migrations",
+	dialect: "postgresql",
+	schemaFilter: ["public"],  // Focus only on public schema for migrations
+	tablesFilter: ["kathyclean_*"],  // Only include your application tables
+	dbCredentials: {
+		url: nonPoolingUrl,
+	},
+	entities: {
+		roles: {
+			provider: "supabase",
+		},
+	},
+});
