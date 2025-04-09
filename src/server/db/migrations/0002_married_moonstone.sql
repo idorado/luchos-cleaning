e9d2c1ebf487b9fef2e547d1ca6e06df51ca84a4
Jose Sanchez <engineer@copydataai.com>
2025-04-09 08:29:59 +0100

    feat(db): add trigger user_created & user_verified

---
+++ src/server/db/migrations/0002_married_moonstone.sql
@@ -0,0 +1,13 @@
+-- Custom SQL migration file, put your code below! --
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+-- trigger the function when a user signs in/their email is confirmed to get missing values
+create trigger on_auth_user_verified
+  after update on auth.users
+  for each row when (
+    old.last_sign_in_at is null
+    and new.last_sign_in_at is not null
+  ) execute procedure public.handle_new_user();
