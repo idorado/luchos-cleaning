49060bd4e2e603a1f93d64dfd172d07d5c55f089
Jose Sanchez <engineer@copydataai.com>
2025-05-29 21:42:37 +0100

    fix: remove hono providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,36 +1,10 @@
 "use client"
 
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
-import { HTTPException } from "hono/http-exception"
-import { type PropsWithChildren, useState } from "react"
+import type { PropsWithChildren } from "react"
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import PostHogPageView from "@/app/posthog-pageview"
 import { useEffect } from 'react'
-
-export const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        queryCache: new QueryCache({
-          onError: (err) => {
-            if (err instanceof HTTPException) {
-              // global error handling, e.g. toast notification ...
-            }
-          },
-        }),
-      })
-  )
-
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
-
 
 export function PostHogProvider({ children }: PropsWithChildren) {
   useEffect(() => {
