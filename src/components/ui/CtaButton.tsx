b6e8075da518104cf40cee190f5852d0f181f539
Jose Sanchez <engineer@copydataai.com>
2025-06-26 22:52:57 +0100

    feat(ui): add CtaButton

---
+++ src/components/ui/CtaButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import type React from 'react';
+
+interface CtaButtonProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const CtaButton = ({ href, children, className }: CtaButtonProps) => {
+  const params = useSearchParams();
+  const query = Object.fromEntries(params.entries());
+  return (
+    <Link
+      href={{
+        pathname: href,
+        query: query,
+      }}
+      className={cn(
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
